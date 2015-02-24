54c519e85b9799539b0aafdee4c106978e21b4ee
lwischik <lu@wischik.com>
2015-02-24 15:53:38 -0800

    Added code that uses a proxy

---
+++ WCF_Examples/UseProxy/Program.cs
@@ -0,0 +1,27 @@
+﻿using Bing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        MainAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task MainAsync()
+    {
+        var bing = new BingSearchContainer(new Uri("https://api.datamarket.azure.com/Bing/Search"));
+        bing.Credentials = new NetworkCredential("accountKey", "QfLwr+UjetjpuH1KDXvIS0+KeHOUp/P7nlWa02KyjDI");
+        var q = bing
+                .Image("big noses", null, null, null, null, null, "Size:Width:1000")
+                .AddQueryOption("$top", 10);
+        var r = await Task.Run(() => q.Execute());
+        Process.Start(r.First().MediaUrl);
+    }
+}
